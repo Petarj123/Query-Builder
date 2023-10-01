9b660e70f5e0268b8a72ef34294efdfaca194991
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-10-01 15:09:46 +0200

    Update UpdateQueiryBuilderIntegrationTest.kt

--- query-builder/src/test/kotlin/com/petar/querybuilder/update/UpdateQueiryBuilderIntegrationTest.kt
+++ query-builder/src/test/kotlin/com/petar/querybuilder/update/UpdateQueiryBuilderIntegrationTest.kt
@@ -27,11 +27,12 @@ class UpdateQueryBuilderIntegrationTest(
             CREATE TABLE $tableName (
                 id SERIAL PRIMARY KEY,
                 email VARCHAR(255) NOT NULL,
-                name VARCHAR(255) NOT NULL
+                name VARCHAR(255) NOT NULL,
+                age INT
             )
         """)
 
-        jdbcTemplate.update("INSERT INTO $tableName(email, name) VALUES('test@example.com', 'Test User');")
+        jdbcTemplate.update("INSERT INTO $tableName(email, name, age) VALUES('test@example.com', 'Test User', 10);")
     }
     @AfterEach
     fun tearDown() {
@@ -39,16 +40,31 @@ class UpdateQueryBuilderIntegrationTest(
     }
     @Test
     fun `update should modify existing row`() {
-        val queryBuilder = UpdateQueryBuilder(tableName, connectionClient)
-        // Act
+        val queryBuilder = createQueryBuilder()
+
         queryBuilder
             .where("email = 'test@example.com'")
             .set("name", "Updated User")
             .execute()
 
-        // Assert
         val result = jdbcTemplate.queryForMap("SELECT * FROM $tableName WHERE email = 'test@example.com'")
         assertEquals("Updated User", result["name"])
 
+    }
+    @Test
+    fun `update should modify age`() {
+        val queryBuilder = createQueryBuilder()
+
+        queryBuilder
+            .where("email = 'test@example.com'")
+            .set("age", 12)
+            .execute()
+
+        val result = jdbcTemplate.queryForMap("SELECT * FROM $tableName WHERE email = 'test@example.com'")
+        assertEquals(12, result["age"])
+    }
+
+    private fun createQueryBuilder(): UpdateQueryBuilder {
+        return UpdateQueryBuilder(tableName, connectionClient)
     }
 }
